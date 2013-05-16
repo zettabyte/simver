d846731accd32ee2b0e907be34c4a1aa61966d21
Kendall Gifford <zettabyte@gmail.com>
2013-05-15 20:54:21 -0600

    Add Simver::Part's #original accessor

--- lib/simver/part.rb
+++ lib/simver/part.rb
@@ -4,14 +4,16 @@ class Simver
 
     VALID = /\A[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*\z/
 
+    attr_reader :parts
+    attr_reader :original
+
     def initialize(part)
-      unless part.to_s =~ VALID
+      @original = part.to_s.freeze
+      unless original =~ VALID
         raise ArgumentError, "invalid format/value for +part+ parameter: #{part.inspect}"
       end
-      @parts = part.to_s.split('.').map { |p| p.freeze }.freeze
+      @parts = original.split('.').map { |p| p.freeze }.freeze
     end
-
-    attr_reader :parts
 
   end
 end
